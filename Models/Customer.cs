8a98459fcc94c973a8d38937cace684b5969a9cd
Amar <ammarkaid321@gmail.com>
2021-11-20 15:34:47 +0300

    Our Home Page only was created, visual only till now. Links will redirect you to some random Pages and the same goes for buttons. However these will be changed later on.

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Go_grocery.Models
+{
+    public class Customer
+    {
+
+        public int MyProperty { get; set; }
+        /*
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        */
+    }
+}
