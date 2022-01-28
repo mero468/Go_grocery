ca14a86156b4403083715cf56822d9c41eba6d24
mero468 <57250365+mero468@users.noreply.github.com>
2022-01-28 22:58:51 +0300

    DATABASE CONTEXT

---
+++ Data/Go_groceryContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Go_grocery.Models;
+
+namespace Go_grocery.Data
+{
+    public class Go_groceryContext : DbContext
+    {
+        public Go_groceryContext (DbContextOptions<Go_groceryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Go_grocery.Models.Product> Product { get; set; }
+
+        public DbSet<Go_grocery.Models.Courier> Courier { get; set; }
+        public DbSet<Go_grocery.Models.ShopOwner> ShopOwner { get; set; }
+        public DbSet<Go_grocery.Models.Customer> Customer { get; set; }
+        public DbSet<Go_grocery.Models.Order> Order  { get; set; }
+
+
+    }
+}
